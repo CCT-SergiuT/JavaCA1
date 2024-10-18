f6c81872264c1f654f31f6c68ea600162856d9a0
CCT-SergiuT <sba24318@student.cct.ie>
2024-10-18 11:38:07 +0100

    adding text files

--- CA1/src/ca1/CA1.java
+++ CA1/src/ca1/CA1.java
@@ -1,20 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ca1;
 
-/**
- *
- * @author sergi
- */
+import java.io.*;
+
+
 public class CA1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
