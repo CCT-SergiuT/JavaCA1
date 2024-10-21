2555e5c36df08ce46c493fb6553efac2b8df7c73
CCT-SergiuT <sba24318@student.cct.ie>
2024-10-21 13:41:52 +0100

    created calculation class with all criteria

--- CA1/src/ca1/CA1.java
+++ CA1/src/ca1/CA1.java
@@ -54,6 +54,48 @@ public class CA1 {
         }
     }
     
-
+    //Create class to calculate the discounts
+    public static double calcFinalValue(Customers customer){
+        //variable of current year
+        int currentYear = 2024;
+        //create variable to keep discount value
+        double discount;
+        //create variable to get purchase year
+        int lastPurchase = customer.getPurchYear();
+        //create variable to get customer class
+        int customerClass = customer.getCustClass();
+        //create variable to get purchase value
+        double totalPurchase = customer.getPurchVal();
+        
+        //using if loop to assign discount value based on conditions
+        //1st criteria - customer calss is 1 and last purchase in 2024 => discount of 30%
+        if(lastPurchase == 2024 && customerClass == 1){
+            discount = 0.30;
+        //2nd criteria - customer calss is 1 and last purchase before 2024 => discount of 20%
+        }else if (lastPurchase < 2024 && lastPurchase >= (currentYear - 5) && customerClass == 1){
+            discount = 0.20;
+        //3rd criteria - customer calss is 1 and last purchase in past 5 years => discount of 10%
+        }else if (lastPurchase < (currentYear - 5) && customerClass == 1){
+            discount = 0.10;
+        //4th criteria - customer calss is 2 and last purchase in 2024 => discount of 15%
+        }else if (lastPurchase == 2024 && customerClass == 2){
+            discount = 0.15;
+        //5th criteria - customer calss is 2 and last purchase before 2024 => discount of 13%
+        }else if (lastPurchase < 2024 && lastPurchase >= (currentYear - 5) && customerClass == 2){
+            discount = 0.13;
+        //6th criteria - customer calss is 2 and no purchase in past 5 years => discount of 5%
+        }else if (lastPurchase < (currentYear - 5) && customerClass == 2){
+            discount = 0.05;
+        //7th criteria - customer calss is 3 and last purchase in 2024 => discount of 3%
+        }else if (lastPurchase == 2024 && customerClass == 3){
+            discount = 0.03;
+        //8th criteria - customer calss is 3 and last purchase before 2024 => no discount
+        }else{
+            discount = 0.00;
+        }
+        
+        //Calculate final value
+        return totalPurchase * (1 - discount);
+    }
     
 }
